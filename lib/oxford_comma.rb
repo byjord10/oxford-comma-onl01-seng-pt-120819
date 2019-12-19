d543a8b43955159a2e93a3a66b384cedd8c51fbd
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-19 02:36:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  array.unshift(array.to_s)
+  if array.length == 2
+    puts "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
